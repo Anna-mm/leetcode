2cda66ffaec3c70576364978170b51ed5814a780
wangfengjiao <wangfengjiao01@baidu.com>
2022-04-29 19:02:28 +0800

    add

---
+++ 2116.判断一个括号字符串是否有效.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=2116 lang=javascript
+ *
+ * [2116] 判断一个括号字符串是否有效
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} locked
+ * @return {boolean}
+ */
+var canBeValid = function(s, locked) {
+
+};
+// @lc code=end
+
